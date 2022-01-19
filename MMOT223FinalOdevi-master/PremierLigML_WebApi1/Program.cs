41ce42514aafa9917c758347e04a2d472dc867a1
yusuftahagunay <97974549+yusuftahagunay@users.noreply.github.com>
2022-01-19 16:36:50 +0300

    Add files via upload

---
+++ MMOT223FinalOdevi-master/PremierLigML_WebApi1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+namespace PremierLigML_WebApi1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Configuration
+            WebHost.CreateDefaultBuilder()
+          .ConfigureServices(services =>
+          {
+                    // Register Prediction Engine Pool
+                    services.AddPredictionEnginePool<PremierLigML.ModelInput, PremierLigML.ModelOutput>().FromFile("PremierLigML.zip");
+          })
+          .Configure(options =>
+          {
+              options.UseRouting();
+              options.UseEndpoints(routes =>
+              {
+                        // Define prediction endpoint
+                        routes.MapPost("/predict", PredictHandler);
+              });
+          })
+          .Build()
+          .Run();
+        }
+
+        static async Task PredictHandler(HttpContext http)
+        {
+            // Get PredictionEnginePool service
+            var predictionEnginePool = http.RequestServices.GetRequiredService<PredictionEnginePool<PremierLigML.ModelInput, PremierLigML.ModelOutput>>();
+
+            // Deserialize HTTP request JSON body
+            var body = http.Request.Body as Stream;
+            var input = await JsonSerializer.DeserializeAsync<PremierLigML.ModelInput>(body);
+
+            // Predict
+            PremierLigML.ModelOutput prediction = predictionEnginePool.Predict(input);
+
+            // Return prediction as response
+            await http.Response.WriteAsJsonAsync(prediction);
+        }
+    }
+}
+
---
+++ MMOT223FinalOdevi-master/Price Prediction/Controllers/PredictionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using PremLig.PredictionML.Model.DataModels;
+
+namespace PremLig.Controllers
+{
+    public class PredictionController : Controller
+    {
+        public IActionResult Price(ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            // Create predection engine related to the loaded train model
+            ITransformer mlModel = mlContext.Model.Load(@"..\Price PredictionML.Model\PremierLigML.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Try model on sample data to predict
+            ModelOutput result = predEngine.Predict(input);
+            ViewBag.Price = result.Score;
+            return View(input);
+        }
+    }
+}
